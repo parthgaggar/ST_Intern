6a2b8d34e674909768a856eb20583bd91b012839
Parth Gaggar <parth.gaggar@gmail.com>
2014-07-01 17:23:35 +0530

    segmented_DAC_weights

--- basic_DAC_estimator.py
+++ basic_DAC_estimator.py
@@ -13,10 +13,9 @@ alpha = 0.001                     #higher order coefficient
 order = 2                       #order of non linearity
 ################Sample and Hold Circuit###############
 dig_out_adc = zeros(no_of_bits)
-no_of_inputs = 20000
+no_of_inputs = 16000
 #################ADC successive approximation#########
 x = [np.random.normal(0,1) for t in arange(no_of_inputs)]
-
 def adc(fx,fx_max,bit_num):
     output=[]
     for each_val in fx:
@@ -35,7 +34,7 @@ def adc(fx,fx_max,bit_num):
     return output
 adc_output = adc(x,max(x),no_of_bits)
 
-no_of_weights = 9 
+no_of_weights = 26 
 error = zeros(no_of_inputs)
 def adaptive_lms_filter(x_input,weight, desired_d,beta):
     L = len(weight)                        
@@ -45,11 +44,15 @@ def adaptive_lms_filter(x_input,weight, desired_d,beta):
     for p in arange(L):
         weight[p] += 2*beta*error*x_input[p]
     return (error,weight)
-
-weights = [2.0**h for h in arange(no_of_weights)]
+no_of_dac_bits = 14
+weights = zeros(no_of_weights)
+for i in arange(8):
+    weights[i] = 2**i * 1.0
+weights[8:11] = 256.0
+weights[11:26] = 1024.0
 dig_input = zeros(no_of_weights)
 #print weights
-dac_coefficients = [np.random.normal(2.0,0.001)**b for b in arange(no_of_weights)]
+dac_coefficients = [weights[b] + np.random.normal(0.0,0.01) for b in arange(no_of_weights)]
 for m in arange(no_of_inputs):
     dig_input[0] = adc_output[m]
     dac_out = sum([dig_input[q]*dac_coefficients[q] for q in arange(no_of_weights)])
